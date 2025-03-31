847a7707a8f037c58c2f0a6c4d43894c7d2893b3
henhenmao <henrymao54321@gmail.com>
2025-03-30 17:23:04 -0700

    Installed axios into the package.json file Accidentally installed axios onto a package.json file in my home directory Fixed the issue (i hope)

--- part2/countries/src/components/Country.jsx
+++ part2/countries/src/components/Country.jsx
@@ -7,7 +7,7 @@ const Country = ({country}) => {
         <div>
             <h1>{country.name.common}</h1>
             <div>Capital: {country.capital}</div>
-            <div>Area: {country.area}</div>
+            <div>{`Area: ${country.area} km²`}</div>
             <h2>Languages</h2>
             <ul>
                 {Object.values(country.languages).map(language => // languages is a object, not an array, cannot use map function, very goofy
